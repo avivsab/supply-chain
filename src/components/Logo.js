466c67a31cd4ceea66b53026c388bb659fa84bf1
Aviv Sebag <avivsab19@gmail.com>
2020-09-21 16:47:19 +0300

    set dinamic content rendering for nav and prepare displaying WH details

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 export default function Logo() {
   return (
-    <svg width="61" height="80">
+    <svg width="61" height="57">
       <rect x="5" y="5" rx="20" ry="20" width="55" height="50"
       style={{fill:'red',stroke:'black',strokeWidth:5,opacity:0.5}}  />
-          <text x="10" y="35" fontFamily="Verdana" fontSize="15" fill="blue">Brand</text>
+          <text x="10" y="35" fontFamily="Verdana" fontSize="15" fill="yellow">Brand</text>
       Sorry, your browser does not support inline SVG.
     </svg>
   )
