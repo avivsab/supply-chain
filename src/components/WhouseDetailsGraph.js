c06605ac66fb56d1c13f0a8d8c30d1ff95553f75
Aviv Sebag <avivsab19@gmail.com>
2020-09-30 13:36:27 +0300

    rotate canvas text and all canvas direction on y axis + adjusments and arrow

--- src/components/WhouseDetailsGraph.js
+++ src/components/WhouseDetailsGraph.js
@@ -15,7 +15,7 @@ export default function WhouseDetailsGraph({ activeWarehouse }) {
             .then(data => {
                 return drawCompData(data)
             })
-            .catch(e =>  {
+            .catch(e => {
                 console.error(`\x1b[43m${e}`)
                 return toggleErr(e)
             })
@@ -51,6 +51,14 @@ export default function WhouseDetailsGraph({ activeWarehouse }) {
         for (i = 0; i < lineLength; ++i) {
             monthX += 100;
             ctx.lineTo(monthX, lineGraph[i]);
+            ctx.save();
+            ctx.font = "20px Arial";
+            ctx.fillStyle = "red";
+            ctx.translate(monthX, lineGraph[i])
+            ctx.rotate(Math.PI)
+            ctx.fillText(lineGraph[i], 10, 0);
+            ctx.rotate(Math.PI * (-1))
+            ctx.restore()
         }
         ctx.stroke();
 
@@ -61,25 +69,40 @@ export default function WhouseDetailsGraph({ activeWarehouse }) {
         monthX = 0;
         let monthesLength = monthGraphNames.length;
         scaleCtx.font = "15px Comic Sans";
-        for (i = 0; i < monthesLength; ++i) {
+        for (i = monthesLength - 1; i >= 0; --i) {
             let currentMonth = monthGraphNames[i];
             scaleCtx.fillText(currentMonth, monthX, 10);
             monthX += 100;
         }
 
+        // arrow line
+        ctx.lineWidth = 5;
+        ctx.strokeStyle = '#008fb0';
+        const middle = canvas.width / 2;
+        ctx.beginPath();
+        ctx.moveTo(middle, 20);
+        ctx.lineTo(middle + 80, 20);
+        ctx.stroke();
+        // arrow head
+        ctx.beginPath();
+        ctx.moveTo(middle + 60, 1);
+        ctx.lineTo(middle + 80, 20);
+        ctx.lineTo(middle + 60, 40);
+        ctx.stroke();
     }
     const [err, handleErr] = React.useState('');
-    const toggleErr = (error) => { setTimeout(() => {
-        if (!error) return;
-        handleErr("Time line can't be shown in graph")
-    }, 500);
-        };
+    const toggleErr = (error) => {
+        setTimeout(() => {
+            if (!error) return;
+            handleErr("Time line can't be shown in graph")
+        }, 500);
+    };
 
 
     // relating to canvas el
     const mainCanvasRef = React.createRef();
     const scaleCanvasRef = React.createRef();
-       
+
     useEffect(() => {
         getMonthesData();
         toggleErr(error);
@@ -88,13 +111,20 @@ export default function WhouseDetailsGraph({ activeWarehouse }) {
 
     const [graphHeadLine, mainCanvasStyle, secondaryCanvasStyle] = [
         { fontFamily: 'Comic', color: 'orange' },
-        { backgroundColor: "lavender", transform: 'skewX(8deg) rotateX(180deg)' },
+        { backgroundColor: "lavender", transform: 'skewX(8deg) rotateX(180deg) rotateY(180deg)' },
         { display: 'block', margin: 'auto', position: 'relative', left: '35px' }
-    ]
+    ];
+    const [quantitySpan, yearSpan] = [
+        { position: 'absolute' },
+        { color: 'cadetblue', zIndex: 1, position: 'relative', bottom: '5px', right: '520px' }
+    ];
+    // graph date
+    const date = new Date();
+    const year = date.getFullYear();
     return (
         <div>
             <h5>Graph View</h5>
-            {err && <h2 style={{color: 'red'}} >{err}</h2>}
+            {err && <h2 style={{ color: 'red' }} >{err}</h2>}
             <h3 style={graphHeadLine}>Stock History Sketch</h3>
             <canvas
                 width={550}
@@ -102,7 +132,8 @@ export default function WhouseDetailsGraph({ activeWarehouse }) {
                 ref={mainCanvasRef}
                 style={mainCanvasStyle}>
             </canvas>
-            <span style={{ position: 'absolute' }}>Stock quantity | (1000 per unit)</span>
+            <span style={quantitySpan}>Stock quantity | (1000 per unit)</span>
+            <span style={yearSpan}>{year}</span>
             <canvas
                 width={550}
                 height={100}
@@ -110,7 +141,6 @@ export default function WhouseDetailsGraph({ activeWarehouse }) {
                 style={secondaryCanvasStyle}
             >
             </canvas>
-
         </div>
     )
 }
