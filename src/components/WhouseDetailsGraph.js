cac69bbdf005a496aca5bc31b0f02de5ef346da3
Aviv Sebag <avivsab19@gmail.com>
2020-10-01 16:08:34 +0300

    merge second solution to graph direction and combine with app

--- src/components/WhouseDetailsGraph.js
+++ src/components/WhouseDetailsGraph.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react'
+import {Button} from 'reactstrap'
 
 export default function WhouseDetailsGraph({ activeWarehouse }) {
     let [currentWH] = activeWarehouse.filter(wh => wh.active);
@@ -12,7 +13,7 @@ export default function WhouseDetailsGraph({ activeWarehouse }) {
             },
         }).then(res => res.json())
             .then(data => {
-                return drawCompData(data)
+                return graphViewFlag ? drawCompData(data) : drawLtrGraph(data);
             })
             .catch(e => {
                 console.error(`\x1b[43m${e}`)
@@ -50,6 +51,7 @@ export default function WhouseDetailsGraph({ activeWarehouse }) {
         const middleX = canvas.width / 2;
         const middleY = canvas.height / 2;
         const ctx = canvas.getContext("2d");
+        ctx.clearRect(0, 0, canvasWidth, canvasHeight);
         ctx.fillStyle = "lightblue";
         ctx.strokeStyle = '#008fb0';
         ctx.lineWidth = 5;
@@ -74,13 +76,14 @@ export default function WhouseDetailsGraph({ activeWarehouse }) {
 
         // drwing secondary canvas
 
-        const scaleSecondaryCanvas = scaleCanvasRef.current;
-        const scaleCtx = scaleSecondaryCanvas.getContext("2d");
+        const secondaryCanvas = secondaryCanvasRef.current;
+        const secondaryCtx = secondaryCanvas.getContext("2d");
+        secondaryCtx.clearRect(0,0,canvasWidth,canvasHeight)
         monthX = 0;
-        scaleCtx.font = "15px Comic Sans";
+        secondaryCtx.font = "15px Comic Sans";
         for (i = monthesLength - 1; i >= 0; --i) {
             let currentMonth = monthGraphNames[i];
-            scaleCtx.fillText(currentMonth, monthX, 10);
+            secondaryCtx.fillText(currentMonth, monthX, 10);
             monthX += 100;
         }
 
@@ -95,6 +98,67 @@ export default function WhouseDetailsGraph({ activeWarehouse }) {
         ctx.lineTo(middleX + 80, 20);
         ctx.lineTo(middleX + 60, 40);
         ctx.stroke();
+    }
+    function drawLtrGraph(data) {
+
+        // getting the fetched data
+
+        const monthInvetoryData = data;
+
+        // and mapping it for using later to draw
+
+        const lineGraph = monthInvetoryData.map(item => item.quantity);
+        const monthGraph = monthInvetoryData.map(item => item.month);
+        const monthGraphNames = monthGraph.map(month => {
+            return new Date(month.toLocaleString("en-us", { month: "short" }))
+        })
+        for (let i = 0; i < monthGraphNames.length; ++i) {
+            monthGraphNames[i] = monthGraphNames[i].toString().substr(3, 5).trim();
+        }
+
+        // scaling the canvas before building and drawing
+
+        const canvasHeight = (Math.max(...lineGraph) + 50);
+        let monthesLength = monthGraphNames.length;
+        const canvasWidth = monthesLength * 100;
+        scaleCanvasWidth(canvasWidth);
+        scaleCanvasHeight(canvasHeight);
+
+        // drawing main alternative canvas
+
+        const ltrCanvas = ltrMainCanvasRef.current;
+        const ltrCtx = ltrCanvas.getContext("2d");
+        ltrCtx.clearRect(0, 0, canvasWidth, canvasHeight);
+        ltrCtx.fillStyle = "lightblue";
+        ltrCtx.font = "20px Arial";
+        ltrCtx.beginPath();
+        ltrCtx.moveTo(0, canvasHeight / 2);
+        let i, lineLength, monthX, currentPoint, currentQuantity;
+        lineLength = lineGraph.length;
+        monthX = 0;
+        // altentive solution first part
+        for (i = 0; i < lineLength; ++i) {
+            monthX += 100
+            currentPoint = Math.abs((canvasHeight-lineGraph[i]));
+            currentQuantity = lineGraph[i];
+            ltrCtx.lineTo(monthX, currentPoint);
+            ltrCtx.fillText(currentQuantity, monthX+20, currentPoint-5)
+        }
+        ltrCtx.stroke();
+
+        // drwing secondary alternative canvas
+
+        const ltrSecondaryCanvas = ltrSecondaryCanvasRef.current;
+        const ltrSecondaryCtx = ltrSecondaryCanvas.getContext("2d");
+        ltrSecondaryCtx.clearRect(0,0,canvasWidth,canvasHeight);
+        monthX = 0;
+        ltrSecondaryCtx.font = "15px Comic Sans";
+        // altentive solution second part - regular looping
+        for (i = 0; i < monthesLength; ++i) {
+            let currentMonth = monthGraphNames[i];
+            ltrSecondaryCtx.fillText(currentMonth, monthX, 10);
+            monthX += 100
+        }
     }
     const [err, handleErr] = React.useState('');
     const toggleErr = (error) => {
@@ -117,7 +181,12 @@ export default function WhouseDetailsGraph({ activeWarehouse }) {
 
     // relating to canvas el
     const mainCanvasRef = React.createRef();
-    const scaleCanvasRef = React.createRef();
+    const secondaryCanvasRef = React.createRef();
+    const ltrMainCanvasRef = React.createRef();
+    const ltrSecondaryCanvasRef = React.createRef();
+
+    const [graphViewFlag, setLtrView] = React.useState(true);
+    const toggleGraph = () => setLtrView(!graphViewFlag)
 
     useEffect(() => {
         getMonthesData();
@@ -129,9 +198,11 @@ export default function WhouseDetailsGraph({ activeWarehouse }) {
         { backgroundColor: "lavender", transform: 'skewX(8deg) rotateX(180deg) rotateY(180deg)' },
         { display: 'block', margin: 'auto', position: 'relative', left: '35px' }
     ];
-    const [quantitySpan, yearSpan] = [
+    const [quantitySpan, yearSpan, regularGraphSpan] = [
         { position: 'absolute' },
-        { color: 'cadetblue', zIndex: 1, position: 'relative', bottom: '5px', right: '520px' }
+        { color: 'cadetblue', zIndex: 1, position: 'relative', bottom: '5px', right: '520px' },
+        { color: 'cadetblue', zIndex: 1, position: 'relative', top: '18px' },
+        
     ];
     // graph date
     const date = new Date();
@@ -142,21 +213,49 @@ export default function WhouseDetailsGraph({ activeWarehouse }) {
             <h5 className="text-info">Graph View</h5>
             {err && <h2 style={{ color: 'red' }} >{err}</h2>}
             <h3 style={graphHeadLine}>Stock History Sketch</h3>
-            <canvas
-                width={width}
-                height={height}
-                ref={mainCanvasRef}
-                style={mainCanvasStyle}>
-            </canvas>
-            <span style={quantitySpan}>Stock quantity | (<b className="text-danger">1000</b> per unit)</span>
-            <span style={yearSpan}>{year}</span>
-            <canvas
-                width={width}
-                height={100}
-                ref={scaleCanvasRef}
-                style={secondaryCanvasStyle}
-            >
-            </canvas>
+            {
+                graphViewFlag ?
+                    <>
+                        <canvas
+                            width={width}
+                            height={height}
+                            ref={mainCanvasRef}
+                            style={mainCanvasStyle}>
+                        </canvas>
+                        <span style={quantitySpan}>Stock quantity | (<b className="text-danger">1000</b> per unit)</span>
+                        <span style={yearSpan}>{year}</span>
+                        <canvas
+                            width={width}
+                            height={100}
+                            ref={secondaryCanvasRef}
+                            style={secondaryCanvasStyle}
+                        >
+                        </canvas>
+                    </>
+                    :
+                    <>
+                        <canvas
+                            width={width}
+                            height={height}
+                            ref={ltrMainCanvasRef}
+                            style={{backgroundColor: "currentColor", transform: 'skewX(8deg)'}}
+                        >
+                        </canvas>
+                        <span style={quantitySpan}>Stock quantity | (<b className="text-danger">1000</b> per unit)</span>
+                        <span style={regularGraphSpan}>{year}</span>
+                        <canvas
+                            width={width}
+                            height={100}
+                            ref={ltrSecondaryCanvasRef}
+                            style={secondaryCanvasStyle}
+                        >
+                        </canvas>
+                    </>
+
+            }
+            
+            <Button color="primary" onClick={toggleGraph}>Switch Direction</Button>
+
         </div>
     )
 }
