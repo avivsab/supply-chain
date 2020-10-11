ec515b8c0ad50ec84d3d2f9bb30f9679956cdbb2
Aviv Sebag <avivsab19@gmail.com>
2020-10-11 15:30:12 +0300

    add violate contract JSON data, config debugger to handle unstructured data

--- src/components/WhouseDetails.js
+++ src/components/WhouseDetails.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
+import UnexpectedDetails from './UnexpectedDetails'
+import Unexpired from './Unexpired';
 
 const Upper = styled.span`
   text-transform: uppercase;
@@ -12,6 +14,7 @@ export class WhouseDetails extends Component {
             product: null,
             quantity: 0,
             expired: null,
+            retrievedData: 'initial'
 
         };
     }
@@ -28,11 +31,28 @@ export class WhouseDetails extends Component {
             },
         }).then(res => res.json())
             .then(data => {
-                this.setState({
-                    product: data.product,
-                    quantity: data.quantity,
-                    expired: data.expired
-                })
+                if(!data.length) {
+                    this.setState({
+                        product: data.product,
+                        quantity: data.quantity,
+                        expired: data.expired,
+                        retrievedData: 'normal'
+                    })
+                }
+                else {
+                    let product, quantity, expierd;
+                    const newState = {};
+                    for(let i = 0; i < data.length; i++) {
+                        product = 'product ' + (i+1);
+                        quantity = 'quantity' + (i+1);
+                        expierd = 'expierd' + (i+1);
+                        newState[product] = data[i].product;
+                        newState[quantity] = data[i].quantity;
+                        newState[expierd] = data[i].expired;                       
+                    }
+                    newState.retrievedData = 'unexpected';
+                    this.setState(newState);
+                }
             })
             .catch(e => {
                 this.setState({error: 'error displaying the data'})
@@ -42,9 +62,22 @@ export class WhouseDetails extends Component {
     }
     async stateToArr() {
         const stateData = await this.getWarehouseStock();
-        console.log(stateData)
         for (const property in stateData) {
-            this.stateArr.push(property, stateData[property]);
+            if(!stateData[property]) continue; // handle unexpected data
+
+        //    handle data missing
+                if(stateData[property] !== 'No expiration date') {
+                    this.stateArr.push(property, stateData[property]);
+                }
+        }
+
+        // handle duplicate data
+        for(let i = 1; i < this.stateArr.length; i++) {
+            if(this.stateArr[i] === this.stateArr[0]) {
+                this.stateArr.splice(0 , i)
+                break;
+            }
+            
         }
     }
     setActiveWH(WH) {
@@ -66,21 +99,31 @@ export class WhouseDetails extends Component {
     render() {
         const { product, quantity, expired, error } = this.state;
         return (
+            this.state.retrievedData === 'unexpected' ?
+            <UnexpectedDetails details={this.stateArr} Upper={Upper}/>
+            :
             <div>   
                     {error ? <h3 style={{color: 'red'}}>Problem getting Warehouse data</h3>
                     :
                     <>          
                     <details style={{position: 'absolute', marginLeft: '50px'}}>
                         <summary>Warehouse available info</summary>
-                        {this.stateArr.map((values, i) => {
-                            return i % 2 === 0 && //  show only keys
-                                (<small className="text-info" style={{ display: 'block', textAlign: 'center', fontSize: '100%' }} key={i}>{values}{i===0 &&'s'} </small>)
+                        {this.stateArr.map((value,i) => {
+                           return i % 2 === 0 &&  //  show only keys
+                                (<small className="text-info" style={{ display: 'block', textAlign: 'center', fontSize: '100%' }} key={i}>{value}{i===0 &&'s'} </small>)
                         })}
                     </details>
                     <section>
                         <div><Upper>product</Upper>:<p className="text-primary">{product}</p></div>
                         <div><Upper>quantity</Upper>:<p className="text-success">{quantity}</p></div>
+                        {expired==='No expiration date'?
+                        <div><Upper>date</Upper>:<p className="text-info">{expired}</p></div>
+                        :
+                        new Date(expired).toUTCString() === 'Invalid Date'?
+                        <Unexpired Upper={Upper}/>
+                        :
                         <div><Upper>date</Upper>:<p className="text-info">{new Date(expired).toUTCString()}</p></div>
+                    }
                     </section>
                     </>
                     }
