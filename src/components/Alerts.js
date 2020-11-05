95e2f0a4ab428d0e2d9a80e599fc3b3258a44337
Aviv Sebag <avivsab19@gmail.com>
2020-11-05 11:41:18 +0200

    add alerts with login/logout, leaft state up and change to usehistory hook

--- src/components/Alerts.js
+++ src/components/Alerts.js
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Alert } from "reactstrap"
+
 import { CompanyDashboard } from './CompanyDashbord'
+import { Login } from './Login'
+import { AlertsList } from './AlertsList'
+
 export const Alerts = (props) => {
-    const passRouter = (props)
+    const intialWarehouses = props.initialWarehouses;
+
+    const [validForm, setValidForm] = useState(false);
+
+    function checkSessionAllreadyValidtate() {
+        if (sessionStorage.getItem('validOnSession') === 'true') {
+            setValidForm(true);
+        }
+    }
+
+    useEffect(() => {
+        checkSessionAllreadyValidtate();
+    }, [])
+
     return (
         <div>
-             <CompanyDashboard rest={passRouter} />
+            <CompanyDashboard warehouses={intialWarehouses} />
             <h1 className="brand-name text-primary">Company Stocks Managment</h1>
+            <div className="container">
+                {!validForm &&
+                <>
+                    <Alert color='primary' className="font-weight-bold">Currently OK
+                    <br />
+                        To see logs fill out the fields
+                    </Alert>
+                    <Login passValidityCheck={setValidForm} />
+                </>
+                }
+                {validForm &&
+                    <AlertsList {...props} />
+                }
+            </div>
         </div>
     )
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -5,18 +5,14 @@ import WhouseDetails from './WhouseDetails'
 import WhouseDetailsGraph from './WhouseDetailsGraph'
 import CompanyDashboard from './CompanyDashbord'
 export const Main = (props) => {
-    const passRouter = (props);
-    const [warehouses, setActive] = useState([
-        { name: 'north', active: false, useCase: 'normal data' },
-        { name: 'west', active: false, useCase: 'big numbers data' },
-        { name: 'east', active: false, useCase: 'violate contract properties' },
-        { name: 'south', active: false, useCase: 'duplicate data' }
-    ])
+    // const passRouter = props.path;
+    const initialWarhouses = props.initialWarehouses;
+    const [warehouses, setActive] = useState(initialWarhouses);
     const [showGraph, toggleGraph] = useState(false);
     const toggleView = () => toggleGraph(!showGraph);
     return (
         <div>
-            <CompanyDashboard warehouses={warehouses} passActiveWarehouse={setActive} rest={passRouter} />
+            <CompanyDashboard warehouses={warehouses} passActiveWarehouse={setActive} />
             <h1 className="brand-name text-primary">Company Stocks Managment</h1>
             {warehouses.map(wh => {
                 return (
