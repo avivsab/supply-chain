10dd4b574165b87bae0e65b8584a6b88cfaaa869
Aviv Sebag <avivsab19@gmail.com>
2020-11-08 11:10:29 +0200

    Updates

---
+++ precache-manifest.832807520d4c13f446cf189d36514a3d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d975a82781b9e4e539df29e5aec9955",
+    "url": "/supply-chain/index.html"
+  },
+  {
+    "revision": "ab6aa4711e7d3b1cce11",
+    "url": "/supply-chain/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "0827fbea22df52f5bc9c",
+    "url": "/supply-chain/static/css/main.18045e65.chunk.css"
+  },
+  {
+    "revision": "ab6aa4711e7d3b1cce11",
+    "url": "/supply-chain/static/js/2.f48b1455.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/supply-chain/static/js/2.f48b1455.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0827fbea22df52f5bc9c",
+    "url": "/supply-chain/static/js/main.fe12c7fb.chunk.js"
+  },
+  {
+    "revision": "297938b7e91d03b9c3e4",
+    "url": "/supply-chain/static/js/runtime-main.bdedd196.js"
+  }
+]);
