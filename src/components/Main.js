ef38d12be487a7e32d9e18c3c70458e060b3da02
Aviv Sebag <avivsab19@gmail.com>
2020-11-15 04:44:19 +0200

    set chart in main prod page

---
+++ src/components/Main.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+import { Button } from 'reactstrap';
+import './main.css'
+import WhouseDetails from './WhouseDetails'
+import CanvasGraphicDetails from './CanvasGraphicDetails'
+import CompanyDashboard from './CompanyDashbord'
+import Chart from './Chart'
+export const Main = (props) => {
+    // const passRouter = props.path;
+    const initialWarhouses = props.initialWarehouses;
+    const passVirtulRoute = props.passVirtulRoute;
+    const currentRoute = props.currentRoute
+    const [warehouses, setActive] = useState(initialWarhouses);
+    const [showGraph, toggleGraph] = useState(false);
+    const toggleView = () => toggleGraph(!showGraph);
+    return (
+        <div>
+            <CompanyDashboard warehouses={warehouses} passActiveWarehouse={setActive} passVirtulRoute={passVirtulRoute} currentRoute={currentRoute} />
+            <h1 className="brand-name text-primary">Company Stocks Managment</h1>
+            {warehouses.map(wh => {
+                return (
+                    wh.active &&
+                    <div key={wh.name}>
+                        <h2 style={{textTransform: 'capitalize'}}>warehouse {wh.name}</h2>
+                        {
+                            !showGraph ?
+                                <>
+                                    <WhouseDetails activeWarehouse={warehouses} />
+                                    <Button color="success" className="view-button" onClick={toggleView}>Graphic View</Button>
+                                </>
+                                :
+                                <>
+                                    <CanvasGraphicDetails activeWarehouse={warehouses} />
+                                    <Button color="info" className="view-button" onClick={toggleView}>Display Details</Button>
+                                </>
+                        }
+                    </div>
+                );
+            })
+            }
+            <Chart />
+        </div>
+    )
+}
